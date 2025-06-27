c67d6deb5df5a2dd6a374e48a72d4ea840cd645f
Neto6391 <joseweedodev@gmail.com>
2025-06-27 01:17:51 -0300

    feat: init app with logging

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from config.settings import Settings
+import logging
 
-# aqui sim, em tempo de execução
-settings = Settings()
+def main():
+    settings = Settings()
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"App iniciando com modelo em {settings.onnx_model_path}")
+    # TODO: FEATURES HERE TO BE IMPLEMENTED
+    
+if __name__ == "__main__":
+    main()
